3ff295b7aa2ea717f0369744d64ab77e821364fc
lucy <lucy199043@gmail.com>
2024-09-23 20:48:00 +0800

    version2.1 - fix CI/CD deploy file

--- llm3_chatbot/settings.py
+++ llm3_chatbot/settings.py
@@ -15,6 +15,7 @@ from decouple import config
 #import environ
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wb59w_l884d!l$)theo5j_x&-$t+w(9l^3a+oq*r37c!s#g@55"
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
